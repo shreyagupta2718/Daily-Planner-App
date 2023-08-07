886966af5180aa7e58b152418b20ea33fc864516
Shreya <shreyagupta2.718@gmail.com>
2023-08-06 17:33:03 -0700

    blocks of right height but not arranged acc to start time

--- src/main/ui/BlockVisual.java
+++ src/main/ui/BlockVisual.java
@@ -8,6 +8,7 @@ import java.awt.*;
 // A visual representation of a block as a clickable rectangle (JButton)
 public abstract class BlockVisual extends JButton {
     protected Block block;
+    int height;
 
     protected final float scaleLength = 10.0F; // Scale to convert 24hr decimal time into pixels (1hr -> 10 pixels)
 
@@ -23,6 +24,7 @@ public abstract class BlockVisual extends JButton {
         constraints.gridx = 0;
         constraints.weightx = 1.0;
         constraints.fill = GridBagConstraints.NONE;
-        constraints.gridheight = (int) (block.getLength() * scaleLength);
+        height = (int) (block.getLength() * scaleLength);
+        setPreferredSize(new Dimension(100, height));
     }
 }
