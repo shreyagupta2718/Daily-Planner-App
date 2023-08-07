d0104f3469c70319dc30bdc3017ae51d418e41ac
Shreya <shreyagupta2.718@gmail.com>
2023-08-06 20:31:03 -0700

    schedule bg lines made, gotta change scaleLength

--- src/main/ui/SchedulePanel.java
+++ src/main/ui/SchedulePanel.java
@@ -10,14 +10,24 @@ import java.awt.*;
 public class SchedulePanel extends JPanel {
     private Schedule schedule;
     protected GridBagConstraints constraints;
+    private final int scheduleStartingPixel = 10;
+    private int scheduleEndingPixel;
 
     // EFFECTS: Constructs a GUI for the schedule
     public SchedulePanel(Schedule schedule) {
         this.schedule = schedule;
-        initializeUI();
+        drawBlocks();
     }
 
-    private void initializeUI() {
+    @Override
+    public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    drawHourLines(g);
+    //drawBlocks();
+    }
+
+
+    private void drawBlocks() {
         setLayout(new GridBagLayout());
         constraints = new GridBagConstraints();
 
@@ -25,6 +35,21 @@ public class SchedulePanel extends JPanel {
             ScheduledBlockVisual scheduledBlockVisual = new ScheduledBlockVisual(block, constraints);
             add(scheduledBlockVisual, constraints);
         }
+    }
+
+    // EFFECTS: draws grid with lines corresponding to hour mark
+    private void drawHourLines(Graphics g) {
+        Color save = g.getColor();
+        g.setColor(new Color(	170, 16, 79));
+        int hour = 0;
+        scheduleEndingPixel = getHeight() - 10;
+        int totalPixels = scheduleEndingPixel - scheduleStartingPixel;
+        for (int y = scheduleStartingPixel; y <= scheduleEndingPixel; y += totalPixels/24) {
+            g.drawString(Integer.toString(hour), 0, y);
+            g.drawLine(0, y, getWidth(), y);
+            hour++;
+        }
+        g.setColor(save);
     }
 }
 
